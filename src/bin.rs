89d9f56f8db0c4a8b3064169d30ef2cf177af56e
Dylan Starink <dylan@starink.com>
2022-12-12 10:45:51 -0800

    Bin: Started on init subcommand
    Using clap for its great flexibility and ease of use. Started with
    the init subcommand and basic code flow

--- src/bin.rs
+++ src/bin.rs
@@ -1,5 +1,25 @@
+
+use clap::{Arg, ArgAction, Command};
 use cluesolverlib::hello_world;
 
 fn main() {
-    hello_world();
+    let matches = Command::new("cluesolver")
+            
+        .subcommand(
+            Command::new("init")
+                .about("Start a new Game")
+        )
+        .get_matches();
+
+
+    match matches.subcommand() {
+        Some(("init", sub_matches)) => {
+            todo!("init!")
+        },
+         
+        _ => {
+
+        }
+    }
+    println!("{:?}", matches)
 }
