a2d2042d42f3f70dde08bf4f7f899daf95769cec
Dylan Starink <dylan@starink.com>
2022-12-14 14:21:20 -0800

    Bin: Move User Input into bin.rs

--- src/bin/bin.rs
+++ src/bin/bin.rs
@@ -1,10 +1,19 @@
 use clap::Command;
-use cluesolverlib::player_hand::PlayerHand;
+use cluesolverlib::player_hand::*;
 use std::vec;
+use std::fmt::Display;
+use std::str::FromStr;
+use std::io;
+use error_chain::error_chain;
 
 use cluesolverlib::solver::*;
-use cluesolverlib::user_input::*;
 
+error_chain!{
+    foreign_links {
+        Io(std::io::Error);
+        ParseInt(::std::num::ParseIntError);
+    }
+}
 
 fn main() -> Result<()> {
     let matches = Command::new("cluesolver")
@@ -42,7 +51,8 @@ fn new_game() -> Result<()> {
     let number_of_cards: usize = 
         get_number_from_user("\n\nHow many cards do you have?")?;
 
-    let mut self_hand: PlayerHand = PlayerHand::new(user_name.trim().to_owned(), number_of_cards);
+    let mut self_hand: PlayerHand = 
+        PlayerHand::new(user_name.trim().to_owned(), number_of_cards);
 
 
     println!("\nPlease enter what cards you have:\n");
@@ -74,4 +84,124 @@ fn new_game() -> Result<()> {
     println!("{:?}", deserialized_state);
     
     Ok(())
+}
+
+
+// -------------------------------
+// ------User Input Helpers-------
+// -------------------------------
+
+pub fn get_card_from_user() -> Result<Card> {
+
+    let card_type = get_string_from_user(
+        "What type of card?\nr) Room\nw) Weapon\ns) Suspect", 
+        |user_input| {
+            if user_input.chars().nth(0).is_none() {
+                return false;
+            }
+
+            if user_input.chars().nth(0).unwrap() == 'r' || 
+                user_input.chars().nth(0).unwrap() == 'w' || 
+                user_input.chars().nth(0).unwrap() == 's' {
+                    return true; 
+            }
+
+            return false;
+        })?;
+
+
+    println!("\n\nPlease enter the number of the card you have:");
+
+    loop {
+        if card_type.chars().nth(0).unwrap() == 'r' {
+            // Room Card
+            let selected_room_card = get_list_item_from_user(&mut enum_iterator::all::<Room>())?;
+
+            return Ok(Card::RoomCard(selected_room_card));
+
+        } else if card_type.chars().nth(0).unwrap() == 'w' {
+            // Weapon Card
+            let selected_room_card = get_list_item_from_user(&mut enum_iterator::all::<Weapon>())?;
+
+            return Ok(Card::WeaponCard(selected_room_card));
+
+        } else if card_type.chars().nth(0).unwrap() == 's' {
+            // Suspect Card
+            let selected_room_card = get_list_item_from_user(&mut enum_iterator::all::<Suspect>())?;
+
+            return Ok(Card::SuspectCard(selected_room_card));
+        }
+    }
+}
+
+
+pub fn get_string_from_user<F>(prompt: &str, valid_input: F) -> Result<String> where F: Fn(&str) -> bool {
+    let stdin = io::stdin();
+    let mut user_input = String::new();
+
+    loop {
+        println!("{}",prompt);
+
+        stdin.read_line(&mut user_input)?;
+
+        if valid_input(&user_input) {
+            return Ok(user_input);
+        } else {
+            println!("Invalid Input, Please try again.");
+            user_input = String::new();
+        }
+    }
+}
+
+pub fn get_number_from_user<T: num::Integer + FromStr>(prompt: &str) -> Result<T> {
+    let string_from_user 
+        = get_string_from_user(prompt, |user_input| {
+            user_input.trim().parse::<T>().is_ok()
+        })?;
+
+        let number = string_from_user.trim().parse::<T>();
+
+        match number {
+            Ok(num) => {
+                return Ok(num);
+            } 
+            Err(_) => {
+                panic!("get_number_from_user failed! with {}", string_from_user.trim());
+            }
+        }
+}
+
+pub fn get_list_item_from_user<T>(list: &mut dyn Iterator<Item = T>) -> Result<T> where T: Display + Clone {
+    
+    loop {
+        let mut counter = 0;
+        let mut indexed_list =  Vec::new();
+
+        for item in &mut *list {
+            println!("{}) {}", counter, item);
+            indexed_list.push(item);
+            counter += 1;
+        }
+
+        let stdin = io::stdin();
+        let mut user_input = String::new();
+
+        stdin.read_line(&mut user_input)?;
+
+        match user_input.trim().parse::<usize>() {
+            Ok(num) => {
+                if num >= counter {
+                    println!("{} is not within the index range!\n", num);
+                    continue;
+                }
+
+                return Ok(indexed_list[num].clone());
+            }
+
+            Err(_) => {
+                println!("Error parsing \"{}\"\n", user_input.trim());
+                continue;
+            }
+        }
+    }
 }
--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -1,13 +1,4 @@
 pub mod solver;
 pub mod player_hand;
-pub mod user_input;
 
 
-use error_chain::error_chain;
-
-error_chain!{
-    foreign_links {
-        Io(std::io::Error);
-        ParseInt(::std::num::ParseIntError);
-    }
-}
