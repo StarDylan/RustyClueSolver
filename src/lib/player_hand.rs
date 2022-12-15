08527f17856fbefd3c0bc035eefb4b91a7c5f6fc
Dylan Starink <dylan@starink.com>
2022-12-14 17:31:16 -0800

    PlayerHand: Moved PlayerHand struct to top

--- src/lib/player_hand.rs
+++ src/lib/player_hand.rs
@@ -1,5 +1,49 @@
 use std::{fmt::{self}, collections::{HashSet}};
 
+/// Contains details about what we know / don't know about a 
+/// player's hand.
+/// 
+/// For example, we might know they have a certain suspect card,
+/// so we mark it under the have_suspect field. Same with weapon and
+/// room.
+/// 
+/// String contains a nice user-readable name to differentiate between
+/// hands.
+
+// Callers from outside my crate can't directly construct me
+// or exhaustively match on my fields!
+#[non_exhaustive]
+#[derive(Debug, Clone,serde::Serialize, serde::Deserialize, PartialEq)]
+pub struct PlayerHand {
+    pub player_name: String,
+
+    pub must_have: HashSet<Card>,
+    pub must_not_have: HashSet<Card>
+}
+
+impl PlayerHand {
+    pub fn new(player_name: String) -> PlayerHand {
+        PlayerHand { 
+            player_name, 
+
+            must_have: HashSet::new(), 
+            must_not_have: HashSet::new(),
+        }
+    }
+
+    pub fn is_complete(&self, expected_number_of_cards: usize) -> bool {
+        if self.must_have.len() == expected_number_of_cards {
+            return true;
+        }
+
+        if self.must_not_have.len() == (21 - expected_number_of_cards - self.must_have.len()) {
+            return true;
+        }
+        
+        return false;
+    }
+}
+
 #[derive(Debug, Eq, PartialEq, Hash, Clone, enum_iterator::Sequence,serde::Serialize, serde::Deserialize)]
 pub enum Suspect {
     Mustard,
@@ -79,41 +123,6 @@ impl fmt::Display for Room {
             Room::Study => write!(f, "Study"),
             Room::Library => write!(f, "Library"),
             Room::Billiard => write!(f, "Billiard Room"),
-        }
-    }
-}
-
-/// Contains details about what we know / don't know about a 
-/// player's hand.
-/// 
-/// For example, we might know they have a certain suspect card,
-/// so we mark it under the have_suspect field. Same with weapon and
-/// room.
-/// 
-/// String contains a nice user-readable name to differentiate between
-/// hands.
-
-// Callers from outside my crate can't directly construct me
-// or exhaustively match on my fields!
-#[non_exhaustive]
-#[derive(Debug, Clone,serde::Serialize, serde::Deserialize)]
-pub struct PlayerHand {
-    pub player_name: String,
-
-    pub number_of_cards: usize,
-
-    pub must_have: HashSet<Card>,
-    pub does_not_have: HashSet<Card>
-}
-
-impl PlayerHand {
-    pub fn new(player_name: String, number_of_cards: usize) -> PlayerHand {
-        PlayerHand { 
-            player_name, 
-            number_of_cards,
-
-            must_have: HashSet::new(), 
-            does_not_have: HashSet::new(),
         }
     }
 }
